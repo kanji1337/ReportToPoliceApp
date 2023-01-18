7ab23c2b4799bb8eeb01f3fa8db91c63afb6942e
kanji1337 <danil123.ddd@mail.ru>
2023-01-18 18:39:01 +0300

    add status of report

--- ReportPoliceSite/ViewModel/PoliceReportsViewModel.cs
+++ ReportPoliceSite/ViewModel/PoliceReportsViewModel.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using ReportPoliceSite.ViewModel;
+using System;
 using System.Collections.Generic;
+using System.Windows.Input;
 using System.Xml.Linq;
 using ReportPoliceSite.Model;
 
@@ -10,7 +12,28 @@ namespace ReportPoliceSite.ViewModel
         private readonly ItemHandler _itemHandler;
 
         static Core Core;
+        public string user_nameS;
         public Report Report { get; set; }
+
+        public void GetEveryName(int report_us_idS)
+        {
+            XDocument xdoc = XDocument.Load("UsersDataBase.xml");
+            XElement db = xdoc.Element("ArrayOfUser");
+            if (db != null)
+            {
+                foreach (XElement user in db.Elements("User"))
+                {
+                    XElement user_id = user.Element("ID");
+                    int user_idI = (int)user_id;
+                    if (report_us_idS == user_idI)
+                    {
+                        XElement user_name = user.Element("Name");
+                        user_nameS = (string)user_name;
+                    }
+                }
+            }
+        }
+
         public void GetEveryReportOfUser( ItemHandler _ItemHandler)
         {
             XDocument xdoc = XDocument.Load("ReportsDataBase.xml");
@@ -19,20 +42,53 @@ namespace ReportPoliceSite.ViewModel
             {
                 foreach (XElement report in db.Elements("Report"))
                 {
-                    XElement user_id = report.Element("RequestUserId");
-                    int user_idI = (int)user_id;
+                    XElement report_us_id = report.Element("RequestUserId");
+                    int report_us_idS = (int)report_us_id;
+                    GetEveryName(report_us_idS);
                     XElement id = report.Element("ID");
                     int idI = (int)id;
+                    XElement phone_num = report.Element("PhoneNumber");
+                    Int64 phone_numI = (Int64)phone_num;
                     XElement num_site = report.Element("NumberPoliceSite");
                     string num_siteS = (String)num_site;
                     XElement req_time = report.Element("RequestTime");
                     DateTime req_timeD = (DateTime)req_time;
                     string status = "В рассмотрении";
-                    _ItemHandler.Add(new Item(idI, num_siteS, req_timeD, status));
+                    _ItemHandler.Add(new PoliceItem(idI, user_nameS, req_timeD, phone_numI, status));
                 }
             }
         }
+        public class PoliceItem 
+        {
+            public PoliceItem(int _ID, string _Name, DateTime _RequestTime, Int64 _PhoneNumber, string _Status)
+            {
+                PID = _ID;
+                PName = _Name;
+                PRequestTime = _RequestTime;
+                PhoneNumber = _PhoneNumber;
+                PStatus = _Status;
+            }
 
+            public string  PStatus{ get; set; }
+            public int PID{ get; set; }
+            public string PName { get; set; }
+            public Int64 PhoneNumber { get; set; }
+            public DateTime PRequestTime { get; set; }
+        }
+        public class ItemHandler
+        {
+            public ItemHandler()
+            {
+                Items = new List<PoliceItem>();
+            }
+
+            public List<PoliceItem> Items { get; private set; }
+
+            public void Add(PoliceItem item)
+            {
+                Items.Add(item);
+            }
+        }
         public PoliceReportsViewModel()
         {
             _itemHandler = new ItemHandler();
@@ -40,9 +96,28 @@ namespace ReportPoliceSite.ViewModel
             Core.LoadReportsFromDB();
             GetEveryReportOfUser( _itemHandler);
         }
-        public List<Item> Items
+        public List<PoliceItem> Items
         {
             get { return _itemHandler.Items; }
+        }
+
+        private RelayCommand saveReports;
+
+        public ICommand SaveReports
+        {
+            get
+            {
+                if (saveReports == null)
+                {
+                    saveReports = new RelayCommand(PerformSaveReports);
+                }
+
+                return saveReports;
+            }
+        }
+
+        private void PerformSaveReports(object commandParameter)
+        {
         }
     }
 }
